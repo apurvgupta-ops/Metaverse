d88eba93fcdece1ec68956a8fb596046d9bba067
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2023-04-19 22:24:18 +0530

    new feature section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -4,7 +4,15 @@ const NewFeatures = ({ imgUrl, title, subtitle }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     <div
       className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px]  bg-[#323f5d]`}
-    ></div>
+    >
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    <h1 className="mt-[26px] leading-[30px] text-white text-[24px] font-bold ">
+      Title {title}
+    </h1>
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]">
+      {subtitle}
+    </p>
   </div>
 );
 
