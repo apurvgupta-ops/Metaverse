b0b06d4ada640bed715dd2c6ad6771310bafc1fb
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2023-05-04 23:40:26 +0530

    Insight

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,27 @@
-'use client';
+"use client";
+import { staggerContainer } from "../utils/motion";
+import styles from "../styles";
+import { motion } from "framer-motion";
+import { insights } from "../constants";
+import { TypingText, ExploreCard, TitleText, InsightCard } from "../components";
 
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title="Insight about metaverse" textStyles="text-center" />
+      <div className="mt-[50px] flex flex-col gap-[30px] ">
+        {insights.map((sight, index) => (
+          <InsightCard key={`insight-${index}`} {...sight} index={index + 1} />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
